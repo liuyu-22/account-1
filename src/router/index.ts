9814d81b36386dd0499b7be003812b5cc4f2e49c
liuyu-22 <729467909@qq.com>
2022-06-27 17:42:44 +0800

    添加icon

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,32 +5,31 @@ import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
 
-
 Vue.use(VueRouter);
 
-const routes= [
+const routes = [
   {
-    path:'/',
-    redirect:'/money'
+    path: '/',
+    redirect: '/money'
   },
   {
-    path:'/money',
+    path: '/money',
     component: Money
   },
   {
-    path:'/labels',
-    component:Labels
+    path: '/labels',
+    component: Labels
   },
   {
     path: '/statistics',
     component: Statistics
   },
   {
-    path:'*',
-    component:NotFound
-  },
-
+    path: '*',
+    component: NotFound
+  }
 ];
+
 const router = new VueRouter({
   routes
 });
