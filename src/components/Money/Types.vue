922c2c521704f2e76162038b379a8750020fcaf7
liuyu-22 <729467909@qq.com>
2022-06-29 13:44:07 +0800

    完成typs组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,54 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Types'
-};
+import Vue from "vue";
+import {Component} from 'vue-property-decorator';
+
+
+@Component
+export default class Types extends Vue {
+  type = '-'; //表示支出 '+'表示收入
+  //@Prop(Number)  propA: number | undefined;
+  //prop告诉Vue.xxx不是data 而是prop
+  // Number告诉Vue.xxx是个Number
+  //XXX是属性名 后边的number|undefined 是XXX的编译时类型
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  selectType(type:string) { //typo只能是 '+'和'-'中的一个
+    if (type !== '-' && type !== '+') {
+      throw  new Error('type is unknown')
+    }
+    this.type = type
+  }
+}
+
+// export default {
+//   name: 'Types',
+//   props: ['xxx'],
+//   data(){
+//     return{
+//       type:'-'//表示支出. '+'表示收入
+//     }
+//   },
+//   mounted() {
+//
+//   },
+//   methods:{
+//     selectType(type){ //typo只能是 '+'和'-'中的一个
+//       if (type !== '-' && type !== '+'){
+//         throw  new Error('type is unknown')
+//       }
+//       this.type=type
+//     }
+//   }
+// };
 </script>
 
 <style lang="scss" scoped>
@@ -19,6 +57,7 @@ export default {
   display: flex;
   text-align: center;
   font-size: 24px;
+
   > li {
     width: 50%;
     height: 64px;
@@ -26,6 +65,7 @@ export default {
     justify-content: center;
     align-items: center;
     position: relative;
+
     &.selected::after {
       content: '';
       position: absolute;
