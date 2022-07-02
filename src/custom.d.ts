211be13d9b1ac1079facd1520b0733589e77e81b
liuyu-22 <729467909@qq.com>
2022-07-02 16:51:53 +0800

    getid

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+type RecordItem = {
+  tags: string[]
+  notes: string
+  type: string
+  amount: number // 数据类型 object | string
+  createdAt?: Date  // 还可以写:类 / 构造函数
+}
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,9 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes @update:value="onUpdateNotes"/>
+    <FormItem field-name="备注"
+           placeholder="在这里输入备注"
+           @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
@@ -11,38 +13,50 @@
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
-import Notes from '@/components/Money/Notes.vue';
+import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
+import recordListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
 
-const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
-type Record = {
-  tags: string[]
-  notes: string
-  type: string
-  amount: number // 数据类型 object | string
-  createdAt?: Date  // 类 / 构造函数
-}
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
+
+// if (version === '0.0.1') {
+//   //数据库升级，数据迁移
+//   recordList.forEach(record => {
+//     record.createdAt = new Date(2022, 0, 1);
+//   });
+//   //保存数据
+//   window.localStorage.setItem('recordList', JSON.stringify(recordList));
+// }else if (version === '0.0.2')
+// window.localStorage.setItem('version', '0.0.2');
+
+
 @Component({
-  components: {Tags, Notes, Types, NumberPad}
+  components: {Tags, FormItem, Types, NumberPad}
 })
 export default class Money extends Vue {
-  tags = ['衣', '食', '住', '行', '彩票'];
-  recordList: Record[] = recordList;
-  record: Record = {
+  tags = tagList;
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
+
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
+
   saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = JSON.parse(JSON.stringify(this.record));
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
+
   @Watch('recordList')
   onRecordListChange() {
     window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
