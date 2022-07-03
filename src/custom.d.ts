2478d0b43c3e0963557cd3ed5a496fc1921f99a2
liuyu-22 <729467909@qq.com>
2022-07-03 15:43:42 +0800

    window.create封装

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,26 @@ type RecordItem = {
   amount: number // 数据类型 object | string
   createdAt?: Date  // 还可以写:类 / 构造函数
 }
+
+interface Window{
+  tagList:Tag[]
+}
+
+type Tag = {
+  id: string;
+  name: string;
+}
+type TagListModel = {
+  data: Tag[]
+  fetch: () => Tag[]
+  create: (name: string) => 'success' | 'duplicated' // 联合类型
+  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+  remove: (id: string) => boolean
+  save: () => void
+}
+
+interface Window{
+  tagList: Tag[];
+  createTag:(name:string)=> void
+
+}
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -32,10 +32,8 @@ export default class Labels extends Vue {
     const name = window.prompt('请输入新标签名');
     if (name) {
       const message = tagListModel.create(name);
-      if (message === 'duplicated') {
-        window.alert('标签名重复了');
-      } else if (message === 'success') {
-        window.alert('添加成功');
+      if (name){
+        window.craeteTag(name)
       }
     }
   }
